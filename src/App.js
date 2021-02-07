38217873c596d80b547bfd354634eba7a0ea79e3
Uluç Bilecen <ubilecen@deloitte.com>
2021-02-07 17:17:54 +0300

    convert to class based component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-
-  const [ personState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Aylin', age: 30 },
       { name: 'Uluc ', age: 35 },
       { name: 'Sherlock', age: 1 },
     ]
-  });
+  }
 
-  const switchNameHandler = () => {
-    //console.log('Was clicked!')
-    setPersonsState({persons: [
+  switchNameHandler = () => {
+    console.log('Was clicked!')
+    this.setState({persons: [
       { name: 'Aylin', age: 30 },
       { name: 'Uluc ', age: 35 },
-      { name: 'Sherlock', age: 2 },
+      { name: 'Sherlock', age: 2},
       ]
     })
   }
 
-  const [otherState,setOtherState] = useState('some other value');
-
-  console.log(personState,otherState);
-
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
-      <Person name={personState.persons[1].name} age={personState.persons[1].age}>My hobbies: Scuba Diving</Person>
-      <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App</h1>
+       <p>This is really working!</p>
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Scuba Diving</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
